ea330851e5f1eab41ea2cb17c7d7dab6b97dc904
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2021-11-09 16:15:25 -0600

    adding starry background and card animations

--- blog/public/js/home.js
+++ blog/public/js/home.js
@@ -11,11 +11,18 @@ db.collection("blogs").get().then((blogs) => {
 const createBlog = (blog) => {
     let data = blog.data();
     blogSection.innerHTML += `
-    <div class="blog-card">
+    <div class="section__posts--card" data-scroll>
         <img src="${data.bannerImage}" class="blog-image" alt="">
-        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <h1 class="blog-title" data-splitting>${data.title.substring(0, 100) + '...'}</h1>
         <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
         <a href="/${blog.id}" class="section__posts--card--button btn dark">read</a>
     </div>
     `;
 }
+
+Splitting();
+
+ScrollOut({
+    threshold: .2,
+    once: true
+});
