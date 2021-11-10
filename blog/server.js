1234495d034cd81e828010e39f6287538a612208
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2021-11-09 19:11:33 -0600

    update

--- blog/server.js
+++ blog/server.js
@@ -8,7 +8,7 @@ app.use(express.static(initial_path));
 app.use(fileupload());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path, "index.html"));
+    res.sendFile(path.join(initial_path, "home.html"));
 })
 
 app.listen("3000", () => {
