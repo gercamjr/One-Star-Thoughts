f66420e0784bfcbe22c43b53abd801e3e15ff6fa
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2021-11-09 20:31:33 -0600

    making it better

--- blog/server.js
+++ blog/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 let initial_path = path.join(__dirname, "public");
 const app = express();
