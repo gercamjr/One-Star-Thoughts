041de56a1a97ca1481e20aef5ea45c7403f49c7c
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2021-11-18 18:46:47 -0600

    chaning navbar

--- blog/public/js/nav.js
+++ blog/public/js/nav.js
@@ -1,23 +1,26 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-const navContainer = document.querySelector('.nav__list');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
 auth.onAuthStateChanged((user) => {
     if (user) {
         //user is logged in
-        navContainer.innerHTML += `<li class="nav__item"><a href="/admin" class="nav__link">Dashboard</a></li>
-        <li class="nav__item"><a href="#" onclick="logoutUser()" class="nav__link">Logout</a></li>`
+        navLinks.innerHTML += `<li><a href="/admin">Dashboard</a></li>
+        <li><a class="login-button" href="#" onclick="logoutUser()">Logout</a></li>`
     } else {
-        navContainer.innerHTML += `<li class="nav__item"><a href="/admin" class="nav__link">Login</a></li>`
+        navLinks.innerHTML += `<li><a class="login-button" href="/admin">Login</a></li>`
     }
 })
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
 });
